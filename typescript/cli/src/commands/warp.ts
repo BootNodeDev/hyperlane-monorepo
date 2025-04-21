40df72159c43d6fde8b2d9d7b2c543fec5ac3bcf
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-04-21 16:23:19 -0300

    fix: Accept strategy tolerance arg

--- typescript/cli/src/commands/warp.ts
+++ typescript/cli/src/commands/warp.ts
@@ -411,6 +411,7 @@ export const check: CommandModuleWithContext<{
 export const rebalancer: CommandModuleWithContext<{
   warpRouteId: string;
   checkFrequency: number;
+  strategyTolerance: string;
 }> = {
   command: 'rebalancer',
   describe: 'Run a warp route collateral rebalancer',
@@ -426,8 +427,19 @@ export const rebalancer: CommandModuleWithContext<{
       demandOption: true,
       alias: 'v',
     },
+    strategyTolerance: {
+      type: 'string',
+      description: 'Threshold for balance to be considered unbalanced',
+      demandOption: false,
+      default: '0',
+    },
   },
-  handler: async ({ context, warpRouteId, checkFrequency }) => {
+  handler: async ({
+    context,
+    warpRouteId,
+    checkFrequency,
+    strategyTolerance,
+  }) => {
     logCommandHeader('Hyperlane Warp Rebalancer');
 
     // Instantiates the warp route monitor
@@ -438,7 +450,7 @@ export const rebalancer: CommandModuleWithContext<{
     );
 
     // Instantiates the strategy that will get rebalancing routes based on monitor results
-    const strategy: IStrategy = new Strategy();
+    const strategy: IStrategy = new Strategy(BigInt(strategyTolerance));
 
     // Instantiates the executor that will process rebalancing routes
     const executor: IExecutor = new Executor();
