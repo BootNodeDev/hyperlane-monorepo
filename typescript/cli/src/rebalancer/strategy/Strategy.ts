9e473f81014e60d51184e8777c5d3501b08530ed
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-04-21 10:28:06 -0300

    feat: Simple strat + tests

--- typescript/cli/src/rebalancer/strategy/Strategy.ts
+++ typescript/cli/src/rebalancer/strategy/Strategy.ts
@@ -1,31 +1,70 @@
-import EventEmitter from 'events';
+import { ChainName } from '@hyperlane-xyz/sdk';
 
-import { MonitorEvent } from '../interfaces/IMonitor.js';
-import { IStrategy, StrategyEvent } from '../interfaces/IStrategy.js';
+export type Route = {
+  fromChain: ChainName;
+  toChain: ChainName;
+  amount: bigint;
+};
 
-/**
- * Simple strategy implementation that processes token balances accross chains and emits a StrategyEvent
- * containing if and how rebalancing is has to be applied.
- */
-export class Strategy implements IStrategy {
-  private readonly STRATEGY_EVENT = 'strategy';
-  private readonly emitter = new EventEmitter();
+export class Strategy {
+  getRebalancingRoutes(balances: Record<ChainName, bigint>): Route[] {
+    const entries = Object.entries(balances);
+    const total = entries.reduce((sum, [, balance]) => sum + balance, 0n);
+    const target = total / BigInt(entries.length);
 
-  subscribe(fn: (event: StrategyEvent) => void): void {
-    this.emitter.on(this.STRATEGY_EVENT, fn);
-  }
+    const surpluss: { chain: ChainName; amount: bigint }[] = [];
+    const deficits: { chain: ChainName; amount: bigint }[] = [];
 
-  async handleMonitorEvent(event: MonitorEvent): Promise<void> {
-    // TODO: Implement actual strategy logic
-    // Current implementation is a placeholder used to test something in typescript/cli/src/tests/warp/warp-rebalancer.e2e-test.ts
-    const strategyEvent: StrategyEvent = {
-      route: event.balances.map((b) => ({
-        origin: b.chain,
-        destination: b.chain,
-        token: b.token,
-        amount: b.value,
-      })),
-    };
-    this.emitter.emit(this.STRATEGY_EVENT, strategyEvent);
+    for (const [chain, balance] of entries) {
+      if (balance < target) {
+        deficits.push({ chain, amount: target - balance });
+      } else if (balance > target) {
+        surpluss.push({ chain, amount: balance - target });
+      } else {
+        // Do nothing as the balance is already on target
+      }
+    }
+
+    const routes: Route[] = [];
+
+    while (surpluss.length > 0 && deficits.length > 0) {
+      const surplus = surpluss[0];
+      const deficit = deficits[0];
+      const fromChain = surplus.chain;
+      const toChain = deficit.chain;
+
+      if (surplus.amount > deficit.amount) {
+        routes.push({
+          fromChain,
+          toChain,
+          amount: deficit.amount,
+        });
+
+        deficits.shift();
+
+        surplus.amount -= deficit.amount;
+      } else if (surplus.amount < deficit.amount) {
+        routes.push({
+          fromChain,
+          toChain,
+          amount: surplus.amount,
+        });
+
+        surpluss.shift();
+
+        deficit.amount -= surplus.amount;
+      } else {
+        routes.push({
+          fromChain,
+          toChain,
+          amount: surplus.amount,
+        });
+
+        deficits.shift();
+        surpluss.shift();
+      }
+    }
+
+    return routes;
   }
 }
