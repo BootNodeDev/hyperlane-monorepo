a40894e93568c9cbc3f523d3a2bb28d3f1d9a5ab
Fernando Zavalia <24811313+fzavalia@users.noreply.github.com>
2025-04-22 12:05:18 -0300

    fix: WIP weighted targets

--- typescript/cli/src/rebalancer/strategy/Strategy.ts
+++ typescript/cli/src/rebalancer/strategy/Strategy.ts
@@ -10,7 +10,14 @@ export class Strategy implements IStrategy {
   /**
    * @param tolerance Value used to prevent rebalancing amounts that are already close to the target
    */
-  constructor(private readonly tolerance: bigint = 0n) {}
+  constructor(
+    private readonly tolerance: bigint = 0n,
+    private readonly weights: Record<ChainName, bigint>,
+  ) {
+    if (Object.values(weights).reduce((acc, next) => acc + next, 0n) !== 100n) {
+      throw new Error('Weights must add up to 100');
+    }
+  }
 
   /**
    * Get the optimized routes that will rebalance all chains to the same balance
@@ -19,64 +26,60 @@ export class Strategy implements IStrategy {
     const entries = Object.entries(rawBalances);
     // Get the total balance from all chains
     const total = entries.reduce((sum, [, balance]) => sum + balance, 0n);
-    // Get the average balance
-    const target = total / BigInt(entries.length);
 
-    // Skip rebalancing when the average balance is very small
-    if (target < this.tolerance) {
-      return [];
-    }
+    // How much each chain should have according to the weights
+    const targets = Object.entries(this.weights).reduce(
+      (targets, [chain, weight]) => {
+        targets[chain] = (total * weight) / 100n;
+        return targets;
+      },
+      {} as Record<ChainName, bigint>,
+    );
 
-    const surpluss: { chain: ChainName; amount: bigint }[] = [];
-    const deficits: { chain: ChainName; amount: bigint }[] = [];
+    // Group balances by balances with surplus or deficit
+    const { surpluss, deficits } = entries.reduce(
+      (acc, [chain, balance]) => {
+        const target = targets[chain];
 
-    // Group balances by balances with surplus or deficit.
-    // The tolerance is used to consider "balanced" chains that are already close to the target
-    for (const [chain, balance] of entries) {
-      if (balance < target - this.tolerance) {
-        deficits.push({ chain, amount: target - balance });
-      } else if (balance > target + this.tolerance) {
-        surpluss.push({ chain, amount: balance - target });
-      } else {
-        // Do nothing as the balance is already on target
-      }
-    }
+        if (balance < target) {
+          acc.deficits.push({ chain, amount: target - balance });
+        } else if (balance > target) {
+          acc.surpluss.push({ chain, amount: balance - target });
+        } else {
+          // Do nothing as the balance is already on target
+        }
+
+        return acc;
+      },
+      {
+        surpluss: [] as { chain: ChainName; amount: bigint }[],
+        deficits: [] as { chain: ChainName; amount: bigint }[],
+      },
+    );
 
     const routes: RebalancingRoute[] = [];
 
-    // Keep iterating until all routes have been found
     while (surpluss.length > 0 && deficits.length > 0) {
       const surplus = surpluss[0];
       const deficit = deficits[0];
-      const fromChain = surplus.chain;
-      const toChain = deficit.chain;
 
-      if (surplus.amount > deficit.amount) {
-        routes.push({
-          fromChain,
-          toChain,
-          amount: deficit.amount,
-        });
+      const transferAmount =
+        surplus.amount < deficit.amount ? surplus.amount : deficit.amount;
 
+      routes.push({
+        fromChain: surplus.chain,
+        toChain: deficit.chain,
+        amount: transferAmount,
+      });
+
+      deficit.amount -= transferAmount;
+      surplus.amount -= transferAmount;
+
+      if (!deficit.amount) {
         deficits.shift();
-        surplus.amount -= deficit.amount;
-      } else if (surplus.amount < deficit.amount) {
-        routes.push({
-          fromChain,
-          toChain,
-          amount: surplus.amount,
-        });
+      }
 
-        surpluss.shift();
-        deficit.amount -= surplus.amount;
-      } else {
-        routes.push({
-          fromChain,
-          toChain,
-          amount: surplus.amount,
-        });
-
-        deficits.shift();
+      if (!surplus.amount) {
         surpluss.shift();
       }
     }
